164c64cc4cce04d3c97806d1a475439cb5a3714e
Rohit Sharma <rohit.sharma@maxerience.com>
2024-03-30 13:18:45 +0530

    Screen3 UI

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,7 +1,10 @@
 import LoginPage from "../pages/auth/LoginPage";
+import SignupPage from "../pages/auth/SignupPage";
+import Screen3 from "../common/Screen3";
 
 const AllRoutes = [
   { name: "Login", path: "/", element: <LoginPage />, private: false },
-   
+  { name: "Signup", path: "/signup", element: <SignupPage />, private: false },
+  { name: "Scree3", path: "/screen3", element: <Screen3 />, private: false },
 ];
 export default AllRoutes;
