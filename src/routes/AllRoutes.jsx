4656c70d28df0891f39b6f7b23efd55969a22cb0
Krishna <krishna@maidencube.com>
2024-03-31 19:03:29 +0530

    Merge Changes

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -5,8 +5,13 @@ import ProjectsPage from "../pages/dashboard/Projects";
 import ProjectDetailsPage from "../pages/dashboard/ProjectDetails";
 import UsersPage from "../pages/dashboard/UsersPage";
 import ProjectPage from "../pages/dashboard/ProjectPage";
+import SignupPage from "../pages/auth/SignupPage";
+import Screen3 from "../common/Screen3";
+
 const AllRoutes = [
   { name: "Login", path: "/", element: <LoginPage />, private: false },
+  { name: "Signup", path: "/signup", element: <SignupPage />, private: false },
+  { name: "Scree3", path: "/screen3", element: <Screen3 />, private: false },
   { name: "Organization", path: "/organization", element: <Organization />, private: false },
   { name: "Organization", path: "/projects", element: <ProjectPage />, private: false },
   { name: "ProjectsPage", path: "/projectsPage", element: <ProjectsPage />, private: false },
