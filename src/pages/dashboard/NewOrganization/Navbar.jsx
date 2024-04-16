f894f1a4f1e4935263432a28da0db3c30650627d
Shaikh Abbas <96783076+abbas4web@users.noreply.github.com>
2024-04-16 19:17:53 +0530

    organization modal completed

--- src/pages/dashboard/NewOrganization/Navbar.jsx
+++ src/pages/dashboard/NewOrganization/Navbar.jsx
@@ -4,6 +4,7 @@ import { AppBar, Toolbar, InputBase, IconButton, Badge } from "@mui/material";
 import {
   Search as SearchIcon,
   Notifications as NotificationsIcon,
+  Settings as SettingsIcon,
 } from "@mui/icons-material";
 
 const Navbar = () => {
@@ -11,22 +12,25 @@ const Navbar = () => {
     <AppBar position="static" style={{ background: "#FFF" }} elevation={1}>
       <Toolbar className="flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center h-full">
-          <Typography className="text-black">Organization</Typography>
+          <Typography className="text-black  font-poppins mt-4 md:mt-0">
+            Organization
+          </Typography>
         </div>
         <div className="flex items-center mt-4 md:mt-0 h-full relative mb-2">
           <InputBase
             placeholder="Search"
-            className="p-2 pl-10 rounded-l-lg focus:outline-none focus:bg-gray-900 h-11 text-[12px]"
+            className="p-2 pl-5 rounded-l-lg focus:outline-none focus:bg-gray-900 h-11 text-[12px]"
             style={{ background: "#F7F9FB" }}
           />
           <div className="pl-3 flex items-center pointer-events-none mr-2">
             <SearchIcon
-              className="text-black z-10 p-1 rounded-r-lg cursor-pointer"
+              className="text-black z-10 p-1 rounded-r-lg"
               style={{
                 background: "#F7F9FB",
                 width: "30px",
                 height: "44px",
                 cursor: "pointer",
+                marginLeft: "-12px",
               }}
             />
           </div>
@@ -38,6 +42,17 @@ const Navbar = () => {
             <Badge badgeContent={0} color="error">
               <NotificationsIcon />
             </Badge>
+          </IconButton>
+          <IconButton
+            color="default"
+            className="ml-2"
+            style={{
+              background: "#F7F9FB",
+              borderRadius: "8px",
+              marginLeft: "6px",
+            }}
+          >
+            <SettingsIcon />
           </IconButton>
         </div>
       </Toolbar>
--- src/pages/dashboard/NewOrganization/index.jsx
+++ src/pages/dashboard/NewOrganization/index.jsx
@@ -1,32 +1,48 @@
-import React from "react";
+import { useState } from "react";
 import Navbar from "./Navbar";
+import Modal from "./Modal";
 
 const Index = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div>
       <Navbar />
       <div>
-        <h1 className="font-bold ml-4 mt-6 lg:mt-0 lg:ml-4">Organization</h1>
+        <h1 className="font-bold lg:text-left text-center  mt-4 lg:mt-0 lg:ml-4 md:mt-12 xl:mt-8 font-poppins">
+          Organization
+        </h1>
       </div>
 
       <div className="flex flex-col items-center mt-6">
-        <div className="text-center mt-8 shadow-sm rounded-lg border p-6 mx-4 sm:w-3/4 md:w-2/3  xl:w-1/3">
+        <div className="text-center mt-8 shadow-sm rounded-lg border p-6 mx-4 sm:w-3/4 md:w-2/3">
           <div>
-            <p className="mt-4 text-sm">
+            <p className="mt-4 text-sm font-poppins">
               It seems like you haven't created an organization yet.
               Organizations are essential for managing your data, projects, and
               team collaboration efficiently. By creating an organization,
               you'll unlock a range of features and benefits
             </p>
             <button
-              className="bg-blue-500  text-white py-1.5 px-4 rounded text-sm"
+              className="bg-blue-500 text-white py-1.5 px-4 rounded text-sm font-poppins"
               style={{ marginTop: "40px" }}
+              onClick={toggleModal}
             >
               Create Organization
             </button>
           </div>
         </div>
       </div>
+
+      <Modal
+        open={isModalOpen}
+        onClose={toggleModal}
+        title="Create Organization"
+      ></Modal>
     </div>
   );
 };
