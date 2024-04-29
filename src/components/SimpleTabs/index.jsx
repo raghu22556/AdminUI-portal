bc3426d3a5f53e6e4c0107d510eb4d7ee77bb664
Krishna <krishna@maidencube.com>
2024-04-30 01:31:53 +0530

    Simple Tabs Added.

---
+++ src/components/SimpleTabs/index.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import {
+  Tabs,
+  TabsHeader,
+  TabPanel,
+  Tab,
+  TabsBody,
+} from "@material-tailwind/react";
+
+class NavPills extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: props.active,
+    };
+  }
+  handleChange = (event, active) => {
+    const { onSelect } = this.props.tabs[active];
+    this.setState({ active });
+    if (onSelect && typeof onSelect === "function") onSelect(active);
+  };
+  handleChangeIndex = (index) => {
+    this.setState({ active: index });
+  };
+  render() {
+    const { tabs, alignCenter } = this.props;
+    const CustomTabs = (
+      <Tabs
+        value={0}
+        onChange={this.handleChange}
+        centered={alignCenter}
+      >
+        <TabsHeader>
+          {tabs.map((prop, key) => (
+            <Tab key={key} value={prop.tabButton}>
+              &nbsp;&nbsp;{prop.tabButton}&nbsp;&nbsp;
+            </Tab>
+          ))}
+        </TabsHeader>
+        <TabsBody>
+          {tabs.map((prop, key) => {
+            return (
+              <TabPanel key={key} value={prop.tabButton}>
+                {prop.tabContent}
+              </TabPanel>
+            );
+          })}
+        </TabsBody>
+      </Tabs>
+    );
+    return <div>{CustomTabs}</div>;
+  }
+}
+
+NavPills.defaultProps = {
+  active: 0,
+};
+
+export default NavPills;
