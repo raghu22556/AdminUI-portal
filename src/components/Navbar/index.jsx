d1935f24a9fb0b0df581a2ea48b260c364c00c2a
Rohit Sharma <rohit.sharma@maxerience.com>
2024-06-05 16:35:54 +0530

    Update Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -126,50 +126,11 @@ const Navbar = (props) => {
         </div> */}
         {/* Profile */}
         <section className="flex items-center gap-1.5 sm:gap-3">
-          <div className="text-sm hidden md:flex flex-col">
-            <span className="text-color font-semibold">Admin</span>
-            <span className="text-gray-500 text-xs">MaidenCube.in</span>
-          </div>
-
-          <ClickAwayListener onClickAway={handleTogglePop}>
-            <button onClick={handlePop}>
-              {pop ? (
-                <BsChevronUp className="text-color text-sm cursor-pointer" />
-              ) : (
-                <BsChevronDown className="text-color text-sm cursor-pointer" />
-              )}
-            </button>
-          </ClickAwayListener>
-
-          <img
-            src="https://s3-alpha-sig.figma.com/img/0eba/1822/f3b6e417139fd32b6e85460026150af7?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=P-QDZ8U9MASEc1BV-X5irII9q913cobryW92i29fBe1VFA16ZoTFMBzVTM-9wyjgh5dP7yBj8NFiXIZAyDuL3PIwdSpSXt0i459-cS7ueyqtQ6scZrjxBNter--9~IxQ-ZjO6mLlossbO~ayB5cafGwb08x8BtfvvnvcxzyZr17iDSZsHDOS1LNcxwRdoexZPGvsHyJLYYfhKgqfPf03BZIThKBPpXKk7zGjmzb~Jv6hfvvJi47nGq346aKbwRWU58Dce6MqDuuNQ7GgQHEZ2VlAVqoSuM3IIuYfdlzp8Mr9m~wv4~l1AeTL3mN-Wl-wSBjCG0tUYlcOjJ7Q8NVyEw__"
-            className="icon-bg text-color text-xl w-8 h-8 md:w-9 md:h-9 flex justify-center items-center rounded-md cursor-pointer"
-          />
-
           <div className="icon-bg text-color text-lg sm:text-xl  w-8 h-8 md:w-9 md:h-9 flex justify-center items-center rounded-full p-1.5 cursor-pointer">
             <img src="/Notifications.svg" alt="icon" />
           </div>
-
-          {/* By clicking outside, the drop-down will be closed */}
         </section>
 
-        {/* Popup */}
-        {pop && (
-          <div className="rounded p-3  gap-1.5 w-36 z-30 absolute top-16 sm:top-20 right-3   bg-white text-xs  grid text-left shadow">
-            <span className="cursor-pointer">My Profile</span>
-            <span className="cursor-pointer">Update Profile</span>
-            <span
-              onClick={() => {
-                localStorage.clear();
-                navigate('/');
-                // toast.success("LogOut Success!");
-              }}
-              className="cursor-pointer"
-            >
-              Sign Out
-            </span>
-          </div>
-        )}
         <FullScreen />
         <Popper
           open={open}
@@ -341,10 +302,9 @@ const Navbar = (props) => {
                   class="relative inline-block object-cover object-center w-9 h-9 rounded-full cursor-pointer"
                   data-popover-target="profile-menu"
                 />
-                <div className="">
-                  <p class=" block font-sans text-sm antialiased font-bold leading-normal text-black">
-                    Rohit Sharma
-                  </p>
+                <div className="text-sm hidden md:flex flex-col">
+                  <span className="text-color font-semibold">Admin</span>
+                  <span className="text-gray-500 text-xs">MaidenCube.in</span>
                 </div>
               </div>
 
@@ -370,74 +330,14 @@ const Navbar = (props) => {
                   My Profile
                 </p>
               </button>
-              <button
-                role="menuitem"
-                class="flex w-full cursor-pointer select-none items-center gap-2 pl-1 rounded-md px-3 pt-[9px] pb-2 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
-              >
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M9.48999 1.17C9.10999 -0.39 6.88999 -0.39 6.50999 1.17C6.45326 1.40442 6.34198 1.62213 6.18522 1.80541C6.02845 1.9887 5.83063 2.13238 5.60784 2.22477C5.38505 2.31716 5.1436 2.35564 4.90313 2.33709C4.66266 2.31854 4.42997 2.24347 4.22399 2.118C2.85199 1.282 1.28199 2.852 2.11799 4.224C2.65799 5.11 2.17899 6.266 1.17099 6.511C-0.390006 6.89 -0.390006 9.111 1.17099 9.489C1.40547 9.54581 1.62322 9.65719 1.80651 9.81407C1.98979 9.97096 2.13343 10.1689 2.22573 10.3918C2.31803 10.6147 2.35639 10.8563 2.33766 11.0968C2.31894 11.3373 2.24367 11.5701 2.11799 11.776C1.28199 13.148 2.85199 14.718 4.22399 13.882C4.42993 13.7563 4.66265 13.6811 4.90318 13.6623C5.14371 13.6436 5.38527 13.682 5.60817 13.7743C5.83108 13.8666 6.02904 14.0102 6.18592 14.1935C6.34281 14.3768 6.45419 14.5945 6.51099 14.829C6.88999 16.39 9.11099 16.39 9.48899 14.829C9.54599 14.5946 9.65748 14.377 9.8144 14.1939C9.97132 14.0107 10.1692 13.8672 10.3921 13.7749C10.6149 13.6826 10.8564 13.6442 11.0969 13.6628C11.3373 13.6815 11.57 13.7565 11.776 13.882C13.148 14.718 14.718 13.148 13.882 11.776C13.7565 11.57 13.6815 11.3373 13.6628 11.0969C13.6442 10.8564 13.6826 10.6149 13.7749 10.3921C13.8672 10.1692 14.0107 9.97133 14.1939 9.81441C14.377 9.65749 14.5946 9.546 14.829 9.489C16.39 9.11 16.39 6.889 14.829 6.511C14.5945 6.45419 14.3768 6.34281 14.1935 6.18593C14.0102 6.02904 13.8666 5.83109 13.7743 5.60818C13.682 5.38527 13.6436 5.14372 13.6623 4.90318C13.681 4.66265 13.7563 4.42994 13.882 4.224C14.718 2.852 13.148 1.282 11.776 2.118C11.5701 2.24368 11.3373 2.31895 11.0968 2.33767C10.8563 2.35639 10.6147 2.31804 10.3918 2.22574C10.1689 2.13344 9.97095 1.9898 9.81407 1.80651C9.65718 1.62323 9.5458 1.40548 9.48899 1.171L9.48999 1.17ZM7.99999 11C8.79564 11 9.55871 10.6839 10.1213 10.1213C10.6839 9.55871 11 8.79565 11 8C11 7.20435 10.6839 6.44129 10.1213 5.87868C9.55871 5.31607 8.79564 5 7.99999 5C7.20434 5 6.44128 5.31607 5.87867 5.87868C5.31606 6.44129 4.99999 7.20435 4.99999 8C4.99999 8.79565 5.31606 9.55871 5.87867 10.1213C6.44128 10.6839 7.20434 11 7.99999 11Z"
-                    fill="#90A4AE"
-                  ></path>
-                </svg>
-                <p class="block font-sans text-sm antialiased font-medium leading-normal text-inherit">
-                  Edit Profile
-                </p>
-              </button>
-              <button
-                role="menuitem"
-                class="flex w-full cursor-pointer select-none items-center gap-2 rounded-md px-3 pl-1 pt-[9px] pb-2 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
-              >
-                <svg
-                  width="14"
-                  height="14"
-                  viewBox="0 0 14 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M2 0C1.46957 0 0.960859 0.210714 0.585786 0.585786C0.210714 0.960859 0 1.46957 0 2V12C0 12.5304 0.210714 13.0391 0.585786 13.4142C0.960859 13.7893 1.46957 14 2 14H12C12.5304 14 13.0391 13.7893 13.4142 13.4142C13.7893 13.0391 14 12.5304 14 12V2C14 1.46957 13.7893 0.960859 13.4142 0.585786C13.0391 0.210714 12.5304 0 12 0H2ZM2 2H12V9H10L9 11H5L4 9H2V2Z"
-                    fill="#90A4AE"
-                  ></path>
-                </svg>
-                <p class="block font-sans text-sm antialiased font-medium leading-normal text-inherit">
-                  Inbox
-                </p>
-              </button>
-              <button
-                role="menuitem"
-                class="flex w-full cursor-pointer select-none items-center gap-2 pl-1 rounded-md px-3 pt-[9px] pb-2 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
-              >
-                <svg
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M16 8C16 10.1217 15.1571 12.1566 13.6569 13.6569C12.1566 15.1571 10.1217 16 8 16C5.87827 16 3.84344 15.1571 2.34315 13.6569C0.842855 12.1566 0 10.1217 0 8C0 5.87827 0.842855 3.84344 2.34315 2.34315C3.84344 0.842855 5.87827 0 8 0C10.1217 0 12.1566 0.842855 13.6569 2.34315C15.1571 3.84344 16 5.87827 16 8ZM14 8C14 8.993 13.759 9.929 13.332 10.754L11.808 9.229C12.0362 8.52269 12.0632 7.76679 11.886 7.046L13.448 5.484C13.802 6.249 14 7.1 14 8ZM8.835 11.913L10.415 13.493C9.654 13.8281 8.83149 14.0007 8 14C7.13118 14.0011 6.27257 13.8127 5.484 13.448L7.046 11.886C7.63267 12.0298 8.24426 12.039 8.835 11.913ZM4.158 9.117C3.96121 8.4394 3.94707 7.72182 4.117 7.037L4.037 7.117L2.507 5.584C2.1718 6.34531 1.99913 7.16817 2 8C2 8.954 2.223 9.856 2.619 10.657L4.159 9.117H4.158ZM5.246 2.667C6.09722 2.22702 7.04179 1.99825 8 2C8.954 2 9.856 2.223 10.657 2.619L9.117 4.159C8.34926 3.93538 7.53214 3.94687 6.771 4.192L5.246 2.668V2.667ZM10 8C10 8.53043 9.78929 9.03914 9.41421 9.41421C9.03914 9.78929 8.53043 10 8 10C7.46957 10 6.96086 9.78929 6.58579 9.41421C6.21071 9.03914 6 8.53043 6 8C6 7.46957 6.21071 6.96086 6.58579 6.58579C6.96086 6.21071 7.46957 6 8 6C8.53043 6 9.03914 6.21071 9.41421 6.58579C9.78929 6.96086 10 7.46957 10 8Z"
-                    fill="#90A4AE"
-                  ></path>
-                </svg>
-                <p class="block font-sans text-sm antialiased font-medium leading-normal text-inherit">
-                  Help
-                </p>
-              </button>
+
               <hr class="my-2 border-blue-gray-50" role="menuitem" />
               <button
+                onClick={() => {
+                  localStorage.clear();
+                  navigate('/');
+                  // toast.success("LogOut Success!");
+                }}
                 role="menuitem"
                 class="flex w-full cursor-pointer select-none items-center gap-2 pl-1 rounded-md px-3 pt-[9px] pb-2 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
               >
